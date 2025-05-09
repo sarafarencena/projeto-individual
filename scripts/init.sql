a0025d175bb9bb1262625cbaa0031938b208628a
sarafarencena <smfsapp@gmail.com>
2025-05-09 16:57:00 -0300

    feat: created physical database schema

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,35 +1,32 @@
--- init.sql
-
--- Criar extensão para suportar UUIDs, se ainda não estiver ativada
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
--- Criar tabela de usuários com UUID como chave primária
-CREATE TABLE IF NOT EXISTS users (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  name VARCHAR(100) NOT NULL,
-  email VARCHAR(100) UNIQUE NOT NULL
+CREATE TABLE "users" (
+  "id" integer PRIMARY KEY,
+  "name" varchar,
+  "class" varchar,
+  "course" varchar,
+  "group" varchar,
+  "role" varchar,
+  "email" varchar,
+  "created_at" timestamp
 );
 
--- Inserir 20 usuários com nomes e emails aleatórios
-INSERT INTO users (name, email)
-VALUES 
-  ('Alice Smith', 'alice.smith@example.com'),
-  ('Bob Johnson', 'bob.johnson@example.com'),
-  ('Carol Williams', 'carol.williams@example.com'),
-  ('David Jones', 'david.jones@example.com'),
-  ('Emma Brown', 'emma.brown@example.com'),
-  ('Frank Davis', 'frank.davis@example.com'),
-  ('Grace Wilson', 'grace.wilson@example.com'),
-  ('Henry Moore', 'henry.moore@example.com'),
-  ('Isabella Taylor', 'isabella.taylor@example.com'),
-  ('Jack Lee', 'jack.lee@example.com'),
-  ('Kate Clark', 'kate.clark@example.com'),
-  ('Liam Martinez', 'liam.martinez@example.com'),
-  ('Mia Rodriguez', 'mia.rodriguez@example.com'),
-  ('Noah Garcia', 'noah.garcia@example.com'),
-  ('Olivia Hernandez', 'olivia.hernandez@example.com'),
-  ('Patrick Martinez', 'patrick.martinez@example.com'),
-  ('Quinn Lopez', 'quinn.lopez@example.com'),
-  ('Rose Thompson', 'rose.thompson@example.com'),
-  ('Samuel Perez', 'samuel.perez@example.com'),
-  ('Tara Scott', 'tara.scott@example.com');
+CREATE TABLE "rooms" (
+  "id" int PRIMARY KEY,
+  "id_user" int,
+  "name" varchar,
+  "floor" varchar
+);
+
+CREATE TABLE "bookings" (
+  "id" int PRIMARY KEY,
+  "id_user" varchar,
+  "id_room" varchar,
+  "time" varchar,
+  "created_at" timestamp,
+  "updated_at" timestamp
+);
+
+ALTER TABLE "rooms" ADD FOREIGN KEY ("id_user") REFERENCES "users" ("id");
+
+ALTER TABLE "bookings" ADD FOREIGN KEY ("id_room") REFERENCES "rooms" ("id");
+
+ALTER TABLE "bookings" ADD FOREIGN KEY ("id_user") REFERENCES "rooms" ("id");
