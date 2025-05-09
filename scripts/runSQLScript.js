ce8ec1974ba993ba09d966024877bed8679b9e7e
sarafarencena <smfsapp@gmail.com>
2025-05-09 00:35:10 -0300

    feat: upadated db

--- scripts/runSQLScript.js
+++ scripts/runSQLScript.js
@@ -9,9 +9,9 @@ const pool = new Pool({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: process.env.DB_SSL === "true" ? {
+    rejectUnauthorized: false
+  } : false
 });
 
 const runSQLScript = async () => {
